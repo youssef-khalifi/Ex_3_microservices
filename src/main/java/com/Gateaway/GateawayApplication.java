b3df56f21e0d8a24a6458da83cb12b11810121ec
youssef khalifi <youssefkhalifi767@gmail.com>
2024-10-02 23:28:48 +0100

    intial project

---
+++ src/main/java/com/Gateaway/GateawayApplication.java
@@ -0,0 +1,13 @@
+package com.Gateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateawayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gateaway/GateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
